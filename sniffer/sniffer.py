1c4f0e7d2a4a6799ff0b9e7fecca4c8d7a771f9a
Gabriel Zea <zea@randomlab.net>
2013-10-04 15:33:03 -0500

    basic network sniffer that send msg over OSC

---
+++ sniffer/sniffer.py
@@ -0,0 +1,34 @@
+import sys
+from scapy.all import *
+from OSC import OSCMultiClient, OSCMessage
+
+print " -- SNIFFER 2 OSC --"
+print "[init]"
+
+PORT = 12345
+
+client = OSCMultiClient()
+client.setOSCTarget(("localhost", PORT))
+
+#client.setOSCTargetFromStr('')
+
+print "sendig to: %s" % client.getOSCTargets()
+
+def parsePackets(d):
+    print "got packet!"
+
+    src = d.sprintf("%IP.src%")
+    sport = d.sprintf("%IP.sport%")
+    dst = d.sprintf("%IP.dst%")
+    dport = d.sprintf("%IP.sport%")
+    proto = d.sprintf("%IP.proto%")
+    payload = d.sprintf("%TCP.payload%")
+    
+    message = OSCMessage("/packet")
+    message.append("src", src)
+    message.append("sport", sport)
+    
+    client.send(message)
+    print "OSCmsg sent!"
+
+sniff(iface="wlan0", prn=lambda x: parsePackets(x))
